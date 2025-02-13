efb23979401df012d919582762cb00c2fa6e46fe
RyanNuggets <152134976+RyanNuggets@users.noreply.github.com>
2025-02-13 18:04:23 +0400

    Update template.js

--- template.js
+++ template.js
@@ -1,6 +1,6 @@
 module.exports = {
     ssu: {
-        text: '**Server Status**\nA server startup has just been hosted! The server is now open for all players to join. Please ensure you follow all server rules and enjoy the session. Join instantly by [clicking here](https://policeroleplay.community/join/LARPJ) or join by using code "LARPJ".'
+        text: '**Server Status**\nA server startup has just been hosted! The server is now open for all players to join. Please ensure you follow all server rules and enjoy the session. Join instantly by [clicking here](<https://policeroleplay.community/join/LARPJ>) or join by using code "LARPJ".'
     },
     ssd: {
         text: '**Server Status**\nThe server is now shutting down. Thank you to everyone who joined and participated in the session! While the server may still be accessible, please be aware that no moderators will be present. We appreciate your time and hope to see you in the next one!'
