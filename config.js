775af5bc53e8724ebb7f0345ed2249f14eb7ab8a
RyanNuggets <152134976+RyanNuggets@users.noreply.github.com>
2025-02-13 20:42:57 +0400

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ module.exports = {
     permission_roles: [
         "1335672434690625627",
     ],
-    protected_message_id: "1339597336544018535"
+    protected_message_id: "1339637773753122867"
 };
