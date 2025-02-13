8bd96567640f2014c9806683f9a7bd8f4992de26
RyanNuggets <152134976+RyanNuggets@users.noreply.github.com>
2025-02-13 16:28:53 +0400

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    channel_id: "1339553389331087425",
-    allow_admin: true, // Set to false if u want that ppl with admin perms cant run it
-    permission_roles: [
-        "1339527721478520852",
-    ],
-    protected_message_id: "1339555083989291028"
-};
