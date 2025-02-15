826c7204d8ba6c0ce6e38f50c287cb313a91dbdf
RyanNuggets <152134976+RyanNuggets@users.noreply.github.com>
2025-02-15 13:08:18 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    channel_id: "1337264239051804722",
+    channel_id: "1339785362544922634",
     allow_admin: true, // Set to false if u want that ppl with admin perms cant run it
     permission_roles: [
-        "1335672434690625627",
+        "1335672431892762820",
     ],
     protected_message_id: "1339818309922586634"
 };
